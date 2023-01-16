7b6649ff267c726e9a16dba025f697e513d56d53
Thiago <thiago_murakami@hotmail.com>
2023-01-16 17:40:09 -0300

    add file and create dataset

---
+++ cadastro.sql
@@ -0,0 +1,10 @@
+/*  
+criando banco cadastro
+acentuado no padrao utf8
+*/
+
+CREATE DATABASE cadastro
+
+-- constraints
+DEFAULT CHARACTER SET utf8
+DEFAULT COLLATE utf8_general_ci; --definir charactere
