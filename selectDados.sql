c9150ac5d4d1c640499f18a08514fde009d49dda
Thiago <thiago_murakami@hotmail.com>
2023-01-19 18:52:58 -0300

    add new method

--- selectDados.sql
+++ selectDados.sql
@@ -6,3 +6,10 @@ order by desc;
 select nome, ano from cursos
 where ano != 2016
 order by ano, nome;
+
+-- LIKE = comecao com A% | termina com %A | todos os %A%
+select * from cursos
+where nome like 'A%'
+where nome like '%A'
+where nome like '%A%'
+where nome not like '%A%'
