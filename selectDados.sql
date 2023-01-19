b8374d24e9d14a2c43ed8b6311af7012cae797e3
Thiago <thiago_murakami@hotmail.com>
2023-01-19 18:57:56 -0300

    add wildcard

--- selectDados.sql
+++ selectDados.sql
@@ -7,9 +7,14 @@ select nome, ano from cursos
 where ano != 2016
 order by ano, nome;
 
--- LIKE = comecao com A% | termina com %A | todos os %A%
+-- LIKE = comecao com A%
 select * from cursos
 where nome like 'A%'
+-- termina com %A
 where nome like '%A'
+-- todos os %A%
 where nome like '%A%'
+-- n seja igual
 where nome not like '%A%'
+-- sublinhado exija qualquer caracter 
+where nome like '%A_'
