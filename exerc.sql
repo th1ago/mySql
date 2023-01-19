605cff627eae67f8b9a57a726388c22252cc2986
Thiago <thiago_murakami@hotmail.com>
2023-01-19 20:12:19 -0300

    add exerc

---
+++ exerc.sql
@@ -0,0 +1,20 @@
+/*Uma lista com o nome de todas as gafanhotos mulheres?*/
+
+/*Uma lista com os dados de todos aqueles que nasceram entre 1/jan/2000 e 31/dez/2015?*/
+
+/*Uma lista com o nome de todos os homems que trabalham como Programadores?*/
+
+/*Uma lista com dados de todas as mulheres que nasceram no Brasil e que tem seu nome iniciado
+com e letra J?*/
+
+/*Uma lista com o nome e nacionalidade de todos os homens que tem Silva no nome, nao nasceram 
+no Brasil e pesam menos de 100Kg?*/
+
+/*Qual a maior altura entre gafanhotos homens que moram no Brasil?*/
+
+/*Qual a media de peso dos gafanhotos cadastrados?*/
+
+/*Qual o menor pesoa entre as gafanhotos mulheres que nasceram fora do Brasil
+ e entre 1/jan/1990 e 31/dez/2000?*/
+
+/*Quantas gafanhotos mulheres tem mais de 1.90m de altura?*/
