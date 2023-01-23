d1c6c7be2391f5d9c38be6fc48a42b151511ea11
Thiago <thiago_murakami@hotmail.com>
2023-01-23 13:30:32 -0300

    add FK

--- alterDados.sql
+++ alterDados.sql
@@ -1,27 +1,27 @@
 /*  
-Tabela Pessoas
+Tabela gafanhotos
 */
 -- describe
-desc pessoas;
+desc gafanhotos;
 
 -- adicionando depois do campo nome
-ALTER TABLE pessoas
+ALTER TABLE gafanhotos
 ADD COLUMN profissao varchar(50) AFTER nome;
 
 -- adicionando como primeiro campo
-ALTER TABLE pessoas
+ALTER TABLE gafanhotos
 ADD COLUMN codigo int FIRST;
 
 -- alterando o tamanho do campo profissao
-ALTER TABLE pessoas
+ALTER TABLE gafanhotos
 MODIFY COLUMN profissao varchar(20) not null default '';
 
 -- renomeando o nome coluna profissao para prof
-ALTER TABLE pessoas
+ALTER TABLE gafanhotos
 CHANGE COLUMN profissao prof varchar(20);
 
 -- renomeando tabela
-ALTER TABLE pessoas
+ALTER TABLE gafanhotos
 RENAME TO galera_do_mal;
 
 /*  
@@ -32,3 +32,12 @@ ADD COLUMN idcurso int FIRST;
 
 ALTER TABLE cursos
 ADD PRIMARY KEY(idcurso);
+
+
+-- FK foreign key
+ALTER TABle gafanhotos;
+ADD cursopreferido int;
+
+ALTER TABLE gafanhotos
+ADD FOREIGN KEY (cursopreferido)
+REFERENCES cursos(idcurso)
