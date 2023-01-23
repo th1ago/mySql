f05bf6b39cc76fd6e589d3da1ac9582b932b7b19
Thiago <thiago_murakami@hotmail.com>
2023-01-23 13:55:16 -0300

    add Left and Right

--- joinDados.sql
+++ joinDados.sql
@@ -1,3 +1,14 @@
+-- JOIN
 SELECT gafanhotos.nome, cursos.nome, cursos.ano
 FROM gafanhotos INNER JOIN cursos
+ON cursos.idcurso = gafanhotos.cursopreferido;
+
+-- LEFT dando preferencia a tabela gafanhoto
+SELECT gafanhotos.nome, cursos.nome, cursos.ano
+FROM gafanhotos LEFT JOIN cursos
+ON cursos.idcurso = gafanhotos.cursopreferido;
+
+-- RIGHT dando preferencia a tabela cursos
+SELECT gafanhotos.nome, cursos.nome, cursos.ano
+FROM gafanhotos RIGHT JOIN cursos
 ON cursos.idcurso = gafanhotos.cursopreferido;
